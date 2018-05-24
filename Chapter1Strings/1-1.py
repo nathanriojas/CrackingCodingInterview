e34a4a3242ba11674a884ffcdf0a20e1752f76c5
Nathan Riojas <nriojas.60@gmail.com>
2018-05-24 16:56:13 -0500

    Update 1-1.py

--- Chapter1Strings/1-1.py
+++ Chapter1Strings/1-1.py
@@ -1,5 +1,6 @@
-# Assuming spaces are not counted
+# Cracking the coding interview chapter 1 problem 1: Given a string, determine if each character is unique
 
+# Assuming spaces are not counted
 def isUnique(st):
 	charSet = [] # hold unique characters
 	for char in st: # iterate through each character in string
@@ -7,15 +8,22 @@ def isUnique(st):
 			return False
 		charSet.append(char)
 	return True
-  
-  
-# Alternatively
+# Alternatively  
 '''
 def isUnique(st):
 	string = st.replace(" ","")
 	return len(set(string)) == len(string)
 '''
 
+# If no additional data structures are allowed, does not consider white space when determining if unique
+'''
+def isUnique(st):
+  for charCheck in range(len(st)):
+    for chars in range (len(st)):
+        if charCheck != chars and st[charCheck] == st[chars] and not st[chars].isspace():
+          return False
+  return True
+'''
 def main():
 	testString = "hello world"
 	print(isUnique(testString))
